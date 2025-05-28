d8579885901e8b20672075e531a88ccb6d9c3cde
moussaguillaume <moussaguillaume@hotmail.fr>
2025-05-28 21:39:57 +0200

    fonction de masquage suivant le role+mise en place du token

---
+++ js/auth/signin.js
@@ -0,0 +1,23 @@
+const mailInput = document.getElementById('EmailInput');
+const passwordInput = document.getElementById('PasswordInput');
+const btnSignIn = document.getElementById('btnSignin');
+
+btnSignin.addEventListener('click', checkCredentials);
+
+function checkCredentials() {
+    //ici, il faudra appeler l'API pour vérifier les credentials en BDD
+
+    if (mailInput.value === "test@mail.com" && passwordInput.value === "123") {
+        //Ici faudra recuperer le vrai token
+        const token = "lkeiofivbiviuhotgoitnhbtgibhoinviornvroinrfvrnogvnronoiroi";
+        setToken(token);
+        //placer ce token en cookie
+
+        setCookie(RoleCookieName, "admin", 7);
+        window.location.replace("/"); // Redirection vers la page de compte
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
